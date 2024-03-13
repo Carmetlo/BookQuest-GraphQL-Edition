9fb537fdb0ffba2865ce86bec5ebc1a30dd660ed
Michael Carmelo <michaelcarmelo22@gmail.com>
2024-03-13 00:37:18 -0600

    changed bcrypt versions redeploying

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
-
 const app = express();
 const PORT = process.env.PORT || 3001;
 
